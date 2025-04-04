85c9f1de06821ff817412947fce8b94decf4e897
Anirudho747 <anirudho.sengupta@live.com>
2025-04-04 08:17:25 +0530

    testcase module completed 34

--- src/components/JiraStoryToTestCase.js
+++ src/components/JiraStoryToTestCase.js
@@ -152,34 +152,6 @@ const GenerateManualTestCases = () => {
           />
         </div>
 
-    {/*    <div className="form-group">*/}
-    {/*      <label>*/}
-    {/*        Additional Instructions (Optional):{" "}*/}
-    {/*        <span className="tooltip-container">*/}
-    {/*        <FaInfoCircle className="info-icon" />*/}
-    {/*        <span className="tooltip-text">*/}
-    {/*          Example:*/}
-    {/*          <ul>*/}
-    {/*  <li>Perform <b>boundary testing</b> by using minimum, maximum, and out-of-range values.</li>*/}
-    {/*  <li>Use <b>role-based testing</b> to validate different user roles (Admin, Guest, User).</li>*/}
-    {/*  <li>Conduct <b>exploratory testing</b> to find unexpected edge cases.</li>*/}
-    {/*  <li>Simulate <b>real-world scenarios</b> (e.g., low network speed, high traffic load).</li>*/}
-    {/*  <li>Test with <b>different user inputs</b>, including invalid, empty, and special characters.</li>*/}
-    {/*</ul>*/}
-    {/*<a href="/Additional_Instructions_sample.txt" download="Additional_Instructions.txt" className="download-button">*/}
-    {/*            📄 Download Sample Additional Instructions*/}
-    {/*          </a>*/}
-    {/*        </span>*/}
-    {/*      </span>*/}
-    {/*      </label>*/}
-    {/*      <textarea*/}
-    {/*          value={additionalInstructions}*/}
-    {/*          onChange={(e) => setAdditionalInstructions(e.target.value)}*/}
-    {/*          placeholder="Enter additional test instructions (optional)"*/}
-    {/*      />*/}
-    {/*    </div>*/}
-
-
         <div className="form-group">
           <label>Test Type:</label>
           <div className="checkbox-group">
