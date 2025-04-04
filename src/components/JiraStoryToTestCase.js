e1a5517dfca235d646c8e2f75d65e0a5362cd31e
Anirudho747 <anirudho.sengupta@live.com>
2025-04-04 08:12:46 +0530

    testcase module completed 3

--- src/components/JiraStoryToTestCase.js
+++ src/components/JiraStoryToTestCase.js
@@ -180,7 +180,6 @@ const GenerateManualTestCases = () => {
     {/*    </div>*/}
 
 
-
         <div className="form-group">
           <label>Test Type:</label>
           <div className="checkbox-group">
